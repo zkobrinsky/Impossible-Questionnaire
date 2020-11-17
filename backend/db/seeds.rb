69ed2f9105971a098c59ff5e777f9c76f020947a
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-17 15:45:22 -0500

    created seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Question.destroy_all
+Questionnaire.destroy_all
+
+
+
+5.times do 
+    questionnaire = Questionnaire.create(title: Faker::Company.bs.titleize)
+
+    5.times do
+        questionData = {
+            content: Faker::ChuckNorris.fact.gsub(".", "?"),
+            answer_1: Faker::ChuckNorris.fact,
+            answer_2: Faker::ChuckNorris.fact,
+            answer_3: Faker::ChuckNorris.fact,
+            answer_4: Faker::ChuckNorris.fact,
+            correct_answer: Faker::ChuckNorris.fact
+        }
+        q1 = Question.new(questionData)
+        q1.questionnaire = questionnaire
+        q1.save
+    end
+    questionnaire.save!
+end
+
+
+
+
