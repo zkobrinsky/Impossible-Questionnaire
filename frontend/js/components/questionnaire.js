c2f2d9d3c3a7944b72448e5cabc61e51cdd77932
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-18 16:50:58 -0500

    added description

--- frontend/js/components/questionnaire.js
+++ frontend/js/components/questionnaire.js
@@ -11,6 +11,7 @@ class Questionnaire {
         this.result = obj.result;
         this.featured = obj.featured;
         this.questions = obj.questions;
+        this.description = obj.description;
         Questionnaire.all.push(this)
     }
 
@@ -18,7 +19,8 @@ class Questionnaire {
         const container = document.querySelector("body > div");
             
         container.innerHTML = (
-            `<h1 id="change-this">${this.title}</h1></br>
+            `<h1 class="title" id="q-${this.id}-title">${this.title}</h1>
+            <h3 class="description" id="q-${this.id}-description">${this.description}</h3></br>
             ${this.displayQuestions()}`
         )
         // add a submit button to innerHTML as well
