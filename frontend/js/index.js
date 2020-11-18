f83babb16413b526c2703332217582ce60e838ee
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-18 16:49:59 -0500

    changed temp q to display on init

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,7 +4,7 @@ async function init() {
     const api = await new ApiService (`http://localhost:3000`);
     api.getAllQuestionnaires()
     .then(() => {
-        Questionnaire.all[2].displayQuestionnaire()
+        Questionnaire.all[1].displayQuestionnaire()
     })
     console.log(questionnaires[2])
     // questionnaires[2].displayQuestionnaire()
