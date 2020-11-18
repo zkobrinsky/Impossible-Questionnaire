4422c1a0aad3ef1dde584db1f8cc61704f941b1d
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-18 15:59:45 -0500

    simple init

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,16 +1,27 @@
+init();
+
+function init() {
+    const api = new ApiService (`http://localhost:3000`);
+    api.getAllQuestionnaires();
+    
+    // debugger;
+    // Questionnaire.displayQuestionnaires();
+}
 
 
-const wrongAnswers = document.querySelectorAll("#radio");
-const rightAnswers = document.querySelector("#correct_answer");
 
-wrongAnswers.forEach(answer => {
-    answer.addEventListener('mouseenter', event => {
-        // debugger;
-    })
-})
 
-const api = new ApiService (`http://localhost:3000`)
-.getAllQuestionnaires()
+
+
+// experimenting with moving radios:
+// const wrongAnswers = document.querySelectorAll("#radio");
+// const rightAnswers = document.querySelector("#correct_answer");
+
+// wrongAnswers.forEach(answer => {
+//     answer.addEventListener('mouseenter', event => {
+//         // debugger;
+//     })
+// })
 
 
 
