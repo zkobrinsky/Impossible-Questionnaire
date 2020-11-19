7750bc38a8603f24db6ee457d4422403c101a87b
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-19 13:00:00 -0500

    display index functinality

--- frontend/js/components/questionnaire.js
+++ frontend/js/components/questionnaire.js
@@ -1,9 +1,4 @@
 class Questionnaire {
-    static displayQuestionnaires() {
-        // debugger;
-    }
-
-    static all = [];
 
     constructor(obj) {
         this.id = obj.id;
@@ -15,18 +10,57 @@ class Questionnaire {
         Questionnaire.all.push(this)
     }
 
+    static all = [];
+
+    static displayQuestionnairesIndex() {
+        const container = document.querySelector("body > div");
+            
+        container.innerHTML = this.all.map(q => {
+            return q.displayQuestionnaireCard()
+        }).join("")
+        this.mountCardButtons()
+    }
+
+    static mountCardButtons() {
+        const cards = document.querySelectorAll(".card");
+
+        for (const card of cards) {
+            card.addEventListener('click', (e) => {
+                const q = Questionnaire.all.find(questionnaire => questionnaire.id === parseInt(card.id.slice(2)))
+                q.displayQuestionnaire();
+            })
+        }
+    }
+
+    displayQuestionnaireCard() {
+        return (`
+        <div class="card" style="width: 18rem;" id="q-${this.id}">
+          <div class="card-body">
+            <h5 class="card-title">${this.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${this.description}</h6>
+          </div>
+        </div>
+        `)
+    }
+
+
     displayQuestionnaire() {
         const container = document.querySelector("body > div");
             
         container.innerHTML = (
             `<h1 class="title" id="q-${this.id}-title">${this.title}</h1>
             <h3 class="description" id="q-${this.id}-description">${this.description}</h3></br>
-            ${this.displayQuestions()}`
+            ${this.displayQuestions()}
+            <button type="button" class="btn btn-outline-primary" id="submit-button">Submit</button>`
         )
-        // add a submit button to innerHTML as well
+        const button = document.querySelector("#submit-button")
+        button.addEventListener('click', (e) => {
+            debugger
+        })
     }
 
     displayQuestions() {
+        // talks to Question class
         return this.questions.map(question => {
             const newQ = new Question(question)
             return newQ.displayQuestion()+("</br></br>");
