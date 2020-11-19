bb182bc8e200b17f58160c5e6feda747471fd87f
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-19 12:59:31 -0500

    mounted buttons

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,13 +4,41 @@ async function init() {
     const api = await new ApiService (`http://localhost:3000`);
     api.getAllQuestionnaires()
     .then(() => {
-        Questionnaire.all[1].displayQuestionnaire()
+        findAndDisplayFeatured()
     })
-    console.log(questionnaires[2])
-    // questionnaires[2].displayQuestionnaire()
-    // debugger;
+
+    mountIndexButton();
+    mountHomeButton();
 }
 
+function findAndDisplayFeatured() {
+    // find all featured
+    const featuredQuestionnaires = Questionnaire.all.filter(questionnaire => (questionnaire.featured === true))  
+    // display random from featured  
+    featuredQuestionnaires[Math.floor(Math.random() * featuredQuestionnaires.length)].displayQuestionnaire()
+}
+
+function mountIndexButton() {
+    const indexButton = document.querySelector("#index-button");
+    indexButton.addEventListener('click', (e) => {
+        clearPage();
+        Questionnaire.displayQuestionnairesIndex()
+    })
+}
+
+function mountHomeButton() {
+    const homeButton = document.querySelector("#home-button");
+    homeButton.addEventListener('click', (e) => {
+        clearPage();
+        findAndDisplayFeatured()
+        // debugger
+    })
+
+}
+
+function clearPage() {
+    document.querySelector("body > div").innerHTML = "";
+}
 
 
 
