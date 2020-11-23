61de457e78153e4b9454414e5ab6e51811a19621
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-23 16:15:42 -0500

    removed unused answers method

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -1,14 +1,3 @@
 class Question < ApplicationRecord
     belongs_to :questionnaire
-
-    def answers
-        [
-        self.answer_1,
-        self.answer_2,
-        self.answer_3,
-        self.answer_4,
-        self.correct_answer
-        ]  
-    end
-
 end
